a24fd9fc45ac5a41bc543e0b1430577d20292ea6
Binsanity95 <84565641+Leeselbin@users.noreply.github.com>
2022-01-19 17:34:37 +0900

    #6강

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return <div>안녕3</div>;
+  console.log('App 실행됨');
+  const [num, setNum] = useState(5);
+
+  let sample = [
+    { id: 1, name: '1길동' },
+    { id: 2, name: '2길동' },
+    { id: 3, name: '3길동' },
+    { id: 4, name: '4길동' },
+  ];
+
+  const [users, setUsers] = useState(sample); // 레퍼런스 변경되야 동작!
+
+  const download = () => {
+    // const a = sample.concat({ id: 5, name: '5길동' });
+    // setUsers(a); //강제로 다시그릴려면 [...sample]
+    setUsers([...sample, { id: num, name: '5길동' }]);
+    setNum(num + 1);
+  };
+
+  // 랜더링 시점 = 상태값 변경
+  return (
+    <div>
+      <button onClick={download}>다운로드</button>
+      {users.map((u) => (
+        <h1>
+          {u.id}, {u.name}
+        </h1>
+      ))}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Sub.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Sub = () => {
+  // 함수 제공!!
+
+  return (
+    <div>
+      <h1>Sub입니다.</h1>
+    </div>
+  );
+};
+
+export default Sub;
